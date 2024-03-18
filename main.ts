afb582100bd3f5962315f0ffa49199bf16b40489
Mubeshira <159130265+Mubeshira@users.noreply.github.com>
2024-03-18 22:58:38 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,54 @@
+// object Example
+let sisters ={
+
+sister1:{
+    name: "Nabiha",
+    age: 35,
+    education: "Bachlors"
+},
+
+sister2:{
+    isName: "Wajeeha",
+    isAge: 33,
+    isEducation: "Masters",}
+
+    };
+console.log(sisters);
+console.log(sisters.sister2.isAge);
+console.log(sisters.sister1);
+console.log(sisters.sister1.name);
+
+
+
+
+// Object of array Example
+let employee ={
+      fullName: "Mubeshira Saad",
+      age: 30,
+      religion: "Islam",
+      nationality: "Pakistani",
+      email: "mubeshira.saad2016@hotmail.com",
+      favoriteSubjects:["English", "Math", "Urdu"],
+      payScedule:{
+        month: "February 2024" ,
+        amount: 35000,
+      }
+};
+
+console.log(employee.fullName);
+console.log(employee.email);
+console.log(employee.payScedule.amount);
+console.log(employee.favoriteSubjects[2]);
+console.log(employee.payScedule.month + ", Amount = " + employee.payScedule.amount);
+
+
+
+
+
+
+
+
+
+
+
+
